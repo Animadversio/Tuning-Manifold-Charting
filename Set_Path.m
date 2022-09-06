7b4f7d034a33cc61d98ee9e29ed96ef1f878ea50
binxu.wang <binxu.wang@wustl.edu>
2022-09-06 00:02:55 -0400

    add newpaths

---
+++ Set_Path.m
@@ -0,0 +1,10 @@
+% Set up path for all analysis inside
+addpath("DNN");
+addpath("experiment_ctrl\optimizers");
+addpath("experiment_ctrl\optimizers\geomtry_util");
+addpath("analysis");
+addpath("analysis\libs\Kent_func");
+addpath("analysis\libs\stat_utils");
+addpath("analysis\libs\stats_plot");
+matdir = "O:\Mat_Statistics";
+mat_dir = "O:\Mat_Statistics";
