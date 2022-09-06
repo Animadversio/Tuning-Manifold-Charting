f6d1c01003c39df8c0430113da26e372dfd32888
binxu.wang <binxu.wang@wustl.edu>
2022-09-05 23:59:29 -0400

    add set path for the repo

---
+++ analysis/Set_Path.m
@@ -0,0 +1,7 @@
+% Set up path for all analysis inside
+addpath(".");
+addpath("libs\Kent_func");
+addpath("libs\stat_utils");
+addpath("libs\stats_plot");
+matdir = "O:\Mat_Statistics";
+mat_dir = "O:\Mat_Statistics";
